5422a6362194d8f74fbe2f5baed6cbfb4004a62d
Ubuntu <ubuntu@ip-172-31-53-188.ec2.internal>
2020-11-26 22:06:23 +0000

    up to maps and lookups

---
+++ terraform-basics-docker/Min-Max-Spread/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "terraform-providers/docker"
+      version = "~> 2.7.2"
+    }
+  }
+}
+
+
+provider "docker" {}
+
+resource "null_resource" "dockervol" {
+  provisioner "local-exec" {
+    command = "mkdir noderedvol/ || true && sudo chown -R 1000:1000 noderedvol/"
+  }
+}
+
+resource "docker_image" "nodered_image" {
+  name = "nodered/node-red:latest"
+}
+
+resource "random_string" "random" {
+  count   = local.container_count
+  length  = 4
+  special = false
+  upper   = false
+}
+
+
+resource "docker_container" "nodered_container" {
+  count = local.container_count
+  name  = join("-", ["nodered", random_string.random[count.index].result])
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = var.int_port
+    external = var.ext_port[count.index]
+  }
+  volumes {
+    container_path = "/data"
+    host_path      = "/home/ubuntu/environment/terraform-docker/noderedvol"
+  }
+}
+
+
+
